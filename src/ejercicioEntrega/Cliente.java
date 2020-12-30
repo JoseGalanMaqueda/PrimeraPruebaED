bf2d2ba1588f0b810b216a20cf62ea59a9fd7a21
llyto <llyto@llytogalan>
2020-12-30 13:32:59 +0100

    Creacion clases Clientes y Cuenta

---
+++ src/ejercicioEntrega/Cliente.java
@@ -0,0 +1,51 @@
+package ejercicioEntrega;
+
+public class Cliente {
+
+	//ATRIBUTOS
+
+	private String dni;
+	private String nombre;
+	private String direccion;
+
+	//CONSTRUCTORES
+
+	public Cliente() {
+		dni = "";
+		nombre = "";
+		direccion = "";
+	}
+
+	public Cliente(String dni, String nombre, String direccion) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.direccion = direccion;
+	}
+
+	//GETTERS Y SETTERS
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
---
+++ src/ejercicioEntrega/Cuenta.java
@@ -0,0 +1,51 @@
+package ejercicioEntrega;
+
+public class Cuenta {
+	
+	//ATRIBUTOS
+	
+	private int numeroCuenta;
+	private int saldo;
+	private Cliente cliente;
+	
+	//CONSTRUCTORES
+	
+	public Cuenta() {
+		numeroCuenta = 0;
+		saldo = 100;
+		cliente = new Cliente();
+	}
+	
+	public Cuenta(int numeroCuenta, int saldo, Cliente cliente) {
+		this.numeroCuenta = numeroCuenta;
+		this.saldo = saldo;
+		this.cliente = cliente;
+	}
+	
+	//GETTERS Y SETTERS
+
+	public int getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public void setNumeroCuenta(int numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+}
